aa2c05ff15a85ff4326bc9a7dcfdae2d8879a462
Daniel Tayloe <daniel.tayloe@enspire.com>
2014-06-26 18:14:25 -0400

    Chapter 7 end

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
 end
 
 group :test do
+  gem 'factory_girl_rails', '4.2.0'
   gem 'selenium-webdriver', '2.35.1'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-#new routing
-  root  'static_pages#home' #Defines for us root_path and root_url
-  
-  #Defines for us signup_path and signup_url
+
+  #sets up all actions needed for users
+  resources :users
+  #Defines for us root_path and root_url
+  root  'static_pages#home'   
+  #Defines for us signup_path and signup_url, etc
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
